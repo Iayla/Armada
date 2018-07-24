43eb351f0a8d4096d6b257d73a7607b7439017cf
Tiffany Shanaz <32331601+Iayla@users.noreply.github.com>
2018-07-24 17:56:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="cover.css" rel="stylesheet">
+</head>
+<body>
+  <body class="text-center">
+
+      <div class="cover-container d-flex w-100 h-50 p-50 mx-auto flex-column">
+        <header class="masthead mb-auto">
+          <div class="inner">
+            <div class="inner">
+     <h3 class="masthead-brand">
+
+     </h3>
+     <nav class="nav nav-masthead justify-content-center">
+      <!-- <a class="nav-link active" href="#">Home</a>
+       <a class="nav-link" href="#">Features</a>
+       <a class="nav-link" href="#">Contact</a> -->
+     </nav>
+
+          </div>
+        </header>
+
+      <main role="main" class="inner cover"> <br><br><br><br>
+        <h1 class="cover-heading">It's not what you think it is</h1> <br>
+        <p class="lead">
+        <br></p>
+        <p class="lead">
+          <nav class="nav flex-column">
+               <a class="nav-link active" href="level1.php">Enter the Harbour</a>
+               <a class="nav-link active" href="level2.php">Find a ship for the mission</a>
+               <a class="nav-link" href="#">Locate Armada</a>
+                <a class="nav-link" href="#">level 4</a>
+                 <a class="nav-link" href="#">level 5</a>
+                  <a class="nav-link" href="#">level 6</a>
+                   <a class="nav-link" href="#">level 7</a>
+
+</nav>
+        </p>
+      </main>
+
+
+  <script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </script>
+</body>
+</html>
